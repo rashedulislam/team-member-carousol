9ae2ebc121ad91bf3f95c93effd961f0691a34e6
Md Rashedul Islam <rashedulislam.ruet@gmail.com>
2021-11-19 20:11:43 +0600

    initial commit

---
+++ plugin.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace StTeamMemberCarousol;
+
+/**
+ * Class Plugin
+ *
+ * Main Plugin class
+ * @since 1.2.0
+ */
+class Plugin {
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.2.0
+	 * @access private
+	 * @static
+	 *
+	 * @var Plugin The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 *
+	 * @return Plugin An instance of the class.
+	 */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * widget_scripts
+	 *
+	 * Load required plugin core files.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function widget_scripts() {
+		wp_register_script( 'st-team-member-carousol', plugins_url( '/assets/js/team-member-carousol.js', __FILE__ ), [ 'jquery' ], false, true );
+	}
+
+	/**
+	 * Editor scripts
+	 *
+	 * Enqueue plugin javascripts integrations for Elementor editor.
+	 *
+	 * @since 1.2.1
+	 * @access public
+	 */
+	public function editor_scripts() {
+		add_filter( 'script_loader_tag', [ $this, 'editor_scripts_as_a_module' ], 10, 2 );
+
+		wp_enqueue_script(
+			'st-team-member-carousol-editor',
+			plugins_url( '/assets/js/editor/editor.js', __FILE__ ),
+			[
+				'elementor-editor',
+			],
+			'1.0.0',
+			true
+		);
+	}
+
+	/**
+	 * Force load editor script as a module
+	 *
+	 * @since 1.2.1
+	 *
+	 * @param string $tag
+	 * @param string $handle
+	 *
+	 * @return string
+	 */
+	public function editor_scripts_as_a_module( $tag, $handle ) {
+		if ( 'st-team-member-carousol-editor' === $handle ) {
+			$tag = str_replace( '<script', '<script type="module"', $tag );
+		}
+
+		return $tag;
+	}
+
+	/**
+	 * Include Widgets files
+	 *
+	 * Load widgets files
+	 *
+	 * @since 1.2.0
+	 * @access private
+	 */
+	private function include_widgets_files() {
+		require_once( __DIR__ . '/widgets/team-member-carousol.php' );
+	}
+
+	/**
+	 * Register Widgets
+	 *
+	 * Register new Elementor widgets.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function register_widgets() {
+		// Its is now safe to include Widgets files
+		$this->include_widgets_files();
+
+		// Register Widgets
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Widgets\Team_Member_Carousol() );
+
+	}
+
+	/**
+	 *  Plugin class constructor
+	 *
+	 * Register plugin action hooks and filters
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function __construct() {
+
+		// Register widget scripts
+		add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+
+		// Register widgets
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
+
+		// Register editor scripts
+		add_action( 'elementor/editor/after_enqueue_scripts', [ $this, 'editor_scripts' ] );
+		
+	}
+}
+
+// Instantiate Plugin Class
+Plugin::instance();
---
+++ widgets/team-member-carousol.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace StTeamMemberCarousol\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Team Member Carousol
+ *
+ * Elementor widget for hello world.
+ *
+ * @since 1.0.0
+ */
+class Team_Member_Carousol extends Widget_Base {
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'team-member-carousol';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Team Member Carousol', 'st-team-member-carousol' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-posts-ticker';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	/**
+	 * Retrieve the list of scripts the widget depended on.
+	 *
+	 * Used to set scripts dependencies required to run the widget.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget scripts dependencies.
+	 */
+	public function get_script_depends() {
+		return [ 'st-team-member-carousol' ];
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			[
+				'label' => __( 'Content', 'st-team-member-carousol' ),
+			]
+		);
+
+		
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+
+	}
+
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function content_template() {
+
+	}
+}
